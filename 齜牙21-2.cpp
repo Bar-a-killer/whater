7b337d09d49d28a825890bf89304785516a23d62
其恩 江 <channing0324@gmail.com>
2023-03-09 18:29:34 +0800

    21-2

---
+++ 齜牙21-2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int main()
+{
+	ios::sync_with_stdio(0),cin.tie(0);
+	int n;
+	cin >> n;
+	int pos[n+1];
+	int data[n+1];
+	for(int j=1;j<=n;j++)
+	{
+		pos[j] = j;
+		data[j] = j;
+	}
+	int m;
+	int mod,po,tmp;
+	cin >> m;
+	int k = 1;
+	for(int i=0;i<m;i++)
+	{
+		cin >> mod >> po;
+		if(mod == 1)
+		{
+			while(pos[po]-k > 0 && data[pos[po]-k] == 0)
+				k++;
+			if(pos[po]-k == 0)
+				continue;
+			tmp=data[pos[po]-k];
+			swap(data[pos[po]],data[pos[po]-k]);
+			swap(pos[po],pos[tmp]);
+			k=1;
+		}
+		else if(mod == 0)
+		{
+			data[pos[po]]=0;
+			
+			pos[po] = 0;
+		}
+		// cout << i << ":";
+		// for(int j=1;j<=n;j++)
+			// cout << data[j] << " ";
+		// cout << endl;
+	}
+	for(int j=1;j<=n;j++)
+		if(data[j] != 0)
+			cout << data[j] << " ";
+	cout << endl;
+}
