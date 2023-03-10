7a115e1e0541418081085e59cd96469e14207590
school <channing0324@gmail.com>
2023-03-10 16:08:39 +0800

    2222

--- 齜牙22.cpp
+++ 齜牙22.cpp
@@ -6,7 +6,7 @@ int main()
 	int T,N;
 	deque<int> datas;
 	cin >> T;
-	int tmp,uja1,uja2=0,uja3,ans=0;
+	int tmp,uja1,uja2,uja3,ans=0,pos = 1;
 	while(T--)
 	{
 		cin >> N;
@@ -15,16 +15,27 @@ int main()
 			cin >> tmp;
 			datas.push_back(tmp);
 		}
-		while(datas.size() != 0)
+//		datas.push_back(0);
+		uja1 = datas[0];
+		uja2 = 0;
+		uja3 = datas[1];
+		while(datas.size() > 1)
 		{
 			uja1=datas.front();
 			datas.pop_front();
-			while(uja1>uja2 && uja3>uja2 && uja3!=datas.back())
+			do
 			{
+				cout << "out-" << uja1 << ":" << uja2 << ":" << uja3 << "pos:" << pos << " " << datas[pos] <<endl; 
 				ans++;
-				uja2 = uja3;
-				uja3 =  
-			}
+				uja2 = max(uja3,uja2);
+				pos++;
+				uja3 = datas[pos];
+			}while(uja1>=uja2 && uja3>=uja2 && pos < datas.size());
+			pos = 1;
+			uja2 = 0;
 		}
+		cout << ":" << ans << endl;
+		ans = 0;
+		while(!datas.empty()) datas.pop_back(); 
 	}
 }
