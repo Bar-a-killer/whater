724fd578290cc8729f1044de005b228078b2528c
3060 <channingbar0324@gmail.com>
2023-05-31 20:43:51 +0800

    wahahahahaha is ac codegit add *git add *!

--- 齜牙165.cpp
+++ 齜牙165.cpp
@@ -19,23 +19,33 @@ int main() {
 		For(i,m) {
 			cin >> p1 >> p2;
 			graph[p1].children.push_back(p2);
-			graph[p2].father ++;
+			graph[p2].father++;
 		}
 		queue<int> output;
-		//priority_queue<int> lone;
+		priority_queue<int, vector<int>, greater<int> > lone;
 		for(int i = 0; i<n ; i++) {
 			if(graph[i].exist && graph[i].father == 0) {
+				lone.push(i);
 				graph[i].exist = 0;
-				for(int j : graph[i].children) {
-					graph[j].father--;
-				}
-				output.push(i);
-				i = 0;
 			}
+		}
+		int ttop;
+		while(!lone.empty()) {
+			ttop = lone.top();
+			lone.pop();
+			for(int i : graph[ttop].children)
+				if(graph[i].exist) {
+					if(--graph[i].father == 0) {
+						graph[i].exist = 0;
+						lone.push(i);
+					}
+				}
+			output.push(ttop);
+			//cout << ttop << endl;
 		}
 		bool go = 0;
 		For(j,n) {
-			if(graph[j].exist) 
+			if(graph[j].exist)
 				go = 1;
 		}
 		if(go)
