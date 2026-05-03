ac351275d85366a2bcd41008bd159760bebaff88
3060 <channing0324@gmail.com>
2026-05-03 19:51:51 +0800

    5/3 real

--- colledge/helloworld.cpp
+++ colledge/helloworld.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main() {
     cout << "hello world" << endl;
-    
+    cout << "hello world" << endl;
+    int n;
+
+    cin >> n;
+    for(int i = 0;i < n;i++) {
+        
+    }
     return 0;
 }
